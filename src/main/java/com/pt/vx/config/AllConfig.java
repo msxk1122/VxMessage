826ac65f53f72c9697a6af10a17d949ed41d7efb
msxk1122 <xiaoxuemsxk@foxmail.com>
2024-09-04 22:09:02 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -21,9 +21,9 @@ import java.util.List;
  */
 public class AllConfig {
     public static final String cron = "0 0 8 * * ?"; //分别代表：秒、分、时、日、月、星期
-    public static final String VxAppId = "微信的APPID";
-    public static final String VxAppSecret = "微信的密钥";
-    public static final String WeatherKey = "高德地图key";
+    public static final String VxAppId = "wxf3b8fa9507d90759";
+    public static final String VxAppSecret = "5fb1a3af1ae26eadafb35ff0db89cc07";
+    public static final String WeatherKey = "aeb577161329b6e368b345e64dfedec8";
     public static final String WeatherKey_HeFeng = "和风天气KEY";
 
     //下列所有填写的  true 为开启， false 为不开启 ，#XXXXXX是颜色16进制HEX码（不知道颜色16进制HEX码可以百度）
@@ -73,10 +73,10 @@ public class AllConfig {
         //  注意：日期里面的数字，填正常的数字就行了.比如1就是1，不要填01
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         userList.add(getUser(
-                "这个人的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
+                "oTl0Z6Z4oxm4J4f1I6AqLb7dm_k0", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "r9wWL3Nyn0bkKhJIWQIWfFLCpZ25v89yl2RRaXv3YC4", //要给这个人发送的模板ID
+                "用户", //咋称呼这个人
+                "江苏省南京市浦口区", //这个人的详细地址
                 "南京", //这个人在的城市
                 new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
                 new BirthDay(1999,8,11,false,false),
@@ -85,10 +85,10 @@ public class AllConfig {
         ));
 
         userList.add(getUser(
-                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID",//要给这个人发送的模板ID
-                "这个人的称呼",//咋称呼这个人
-                "江苏省南京市玄武区",//这个人的详细地址
+                "oTl0Z6Z4oxm4J4f1I6AqLb7dm_k0",//扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "r9wWL3Nyn0bkKhJIWQIWfFLCpZ25v89yl2RRaXv3YC4",//要给这个人发送的模板ID
+                "用户",//咋称呼这个人
+                "江苏省南京市浦口区",//这个人的详细地址
                 "南京",//这个人在的城市
                 new BirthDay(1999,8,11,false,false),
                 new BirthDay(1999,2,15,true,false),
